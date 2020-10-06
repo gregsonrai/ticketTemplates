e82560f72daf65048ea5540d78c6f9b3d537e197
Greg <greg.davis@sonraisecurity.com>
2020-10-06 16:08:10 -0300

    Updating to pass in account ID

--- bot001/bot.py
+++ bot001/bot.py
@@ -1,12 +1,24 @@
-import sonrai.platform.aws.arn
-
+import re
+import sys
 import logging
+import sonrai.platform.aws.arn
 
 def run(ctx):
     # Create AWS identity and access management client
-    iam_client = ctx.get_client().get('iam')
+    user_srn = ctx.User_select
+    pattern = 'srn:aws:iam::(\d+).*/(.*)$'
+    a = re.search(pattern, string_one)
 
-    user_name = ctx.User_select
+    if a is None:
+        logging.error('Could not parse AWS SRN {}'.format(user_srn))
+        sys.exit()
+
+
+    account_id = a.group(1)
+    user_name = a.group(2)
+
+    iam_client = ctx.get_client(account_id).get('iam')
+
     tag_key = ctx.TagKey
     tag_value = ctx.TagValue
 
