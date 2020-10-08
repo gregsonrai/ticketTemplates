76fbe3fa3cbb5461ea83782e3130cc404501f445
Greg <greg.davis@sonraisecurity.com>
2020-10-08 12:00:23 -0300

    Adding bot that will add a user to a group in Azure

---
+++ bot003/bot.py
@@ -0,0 +1,54 @@
+import re
+import sys
+import logging
+import sonrai.platform.aws.arn
+
+def run(ctx):
+    # Create Azure identity and access management client
+    ticket = ctx.config.get('data').get('ticket')
+
+    customFields = ticket.get('customFields')
+    user_srn = None
+    group_srn = None
+
+    # Parse all custom fields looking for what we are expecting
+    # in this case, the user SRN is in "User select"
+    # and the group SRN is in "Group select"
+    #
+    for customField in ticket.get('customFields'):
+        name = customField['name']
+        value = customField['value']
+
+        if name == 'User select':
+            user_srn = value
+        elif name == 'Group select':
+            group_srn = value
+
+    # Parse out the user ID and account / tenant ID
+    #
+    pattern = 'srn:azure.*::(.*)?/User/(.*)$'
+    a = re.search(pattern, user_srn)
+
+    if a is None:
+        logging.error('Could not parse User SRN {}'.format(user_srn))
+        sys.exit()
+
+    tenantId = a.group(1)
+    userId = a.group(2)
+
+    # ... and parse out the group ID
+    #
+    pattern = 'srn:azure.*/Group/(.*)$'
+    a = re.search(pattern, group_srn)
+
+    if a is None:
+        logging.error('Could not parse Group SRN {}'.format(user_srn))
+        sys.exit()
+
+    groupId = a.group(1)
+
+    # Get the client and call the API to add a member
+    #
+    logging.info('Adding user {} to group {}'.format(userId, groupId))
+    graphrbac_client = ctx.get_client().get(GraphRbacManagementClient)
+    graphrbac_client.groups.add_member(group_object_id=groupId, url='https://graph.windows.net/' + tenantId + '/directoryObjects/' + userId)
