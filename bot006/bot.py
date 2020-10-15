7c44cfdde073709b2f76cf400621e8c7ec7348c8
Greg <greg.davis@sonraisecurity.com>
2020-10-15 11:47:10 -0300

    Adding new bot for updating trust relationship

--- bot006/bot.py
+++ bot006/bot.py
@@ -12,8 +12,7 @@ def run(ctx):
 
     # Get the list of custom fields from the ticket
     user_srn = None
-    tag_key = None
-    tag_value = None
+    role_srn = None
     duration = None
 
     # Loop through each of the custom fields and set the values that we need
@@ -23,10 +22,8 @@ def run(ctx):
 
         if name == 'User select':
             user_srn = value
-        elif name == 'TagKey':
-            tag_key = value
-        elif name == 'TagValue':
-            tag_value = value
+        elif name == 'Role select':
+            role_srn = value
         elif name == 'Duration hours':
             duration = value
 
@@ -34,21 +31,26 @@ def run(ctx):
     # Note:  Account here is used by the frameworks to know which collector
     #        needs to run the bot
     pattern = 'srn:aws:iam::(\d+).*/(.*)$'
-    a = re.search(pattern, user_srn)
+    a = re.search(pattern, role_srn)
 
     if a is None:
-        logging.error('Could not parse AWS SRN {}'.format(user_srn))
+        logging.error('Could not parse AWS SRN {}'.format(role_srn))
         sys.exit()
 
     account_id = a.group(1)
-    user_name = a.group(2)
+    role_name = a.group(2)
 
     # Get the AWS IAM client from our frameworks
     iam_client = ctx.get_client(account_id = account_id).get('iam')
 
-    # Call the AWS tag_user API to tag the user
-    logging.info('Tagging user {} with {} : {}'.format(user_name, tag_key, tag_value))
-    iam_client.tag_user(UserName=user_name, Tags=[ { 'Key': tag_key, 'Value': tag_value } ])
+    # Get the role so we can get the assume_role_policy_document
+    logging.info('Retrieving role {} in account {}'.format(role_name, account_id))
+    role = iam_client.Role(role_name)
+
+    policy_document = role.assume_role_policy_document
+
+    logging.info("Got the role!  Its policy document is {}".format(policy_document))
+    sys.exit()
 
     # If there is a duration, we need to create a followup ticket to revert the change
     # If there is not, we are done here
